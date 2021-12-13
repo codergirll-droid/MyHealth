3e59d9552cfd7f445b38cc3052fba34608ac1435
U180709025 <uzayisinalici@posta.mu.edu.tr>
2021-12-13 22:28:51 +0300

    update physical health

--- app/src/main/java/com/example/myhealth/PhysicalHealth.java
+++ app/src/main/java/com/example/myhealth/PhysicalHealth.java
@@ -10,13 +10,32 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PhysicalHealth extends AppCompatActivity {
 
-    EditText  ;
+    EditText distanceInKm ;
+    int distance ;
+    EditText stepsCount ;
+    int steps ;
+    EditText ExtraBurnedCalories ;
+    int extraCalories ;
+    EditText totalCalorie ;
+    int totalCalorieIntake ;
     FloatingActionButton backMain ;
-    Button OK_btn ;
+    Button OK_btn1 ;
+    Button OK_btn2 ;
+    Button OK_btn3 ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physical_health);
 
-}}
+        distanceInKm = (EditText) findViewById(R.id.txtDistanceInKm) ;
+        stepsCount = (EditText) findViewById(R.id.txtStepsCount) ;
+        ExtraBurnedCalories = (EditText) findViewById(R.id.txtExtraBurnedCalories) ;
+        totalCalorie = (EditText) findViewById(R.id.txtTotalCalorieIntake) ;
+        
+
+
+
+    }
+}
