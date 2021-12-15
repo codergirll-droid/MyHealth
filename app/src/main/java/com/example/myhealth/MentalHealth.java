593aacb2f6d5c644e540b44d9f01b7f456bdb3a8
U180709025 <uzayisinalici@posta.mu.edu.tr>
2021-12-15 11:45:04 +0300

    mentalHealth class

--- app/src/main/java/com/example/myhealth/MentalHealth.java
+++ app/src/main/java/com/example/myhealth/MentalHealth.java
@@ -1,5 +1,37 @@
 package com.example.myhealth;
 
-public class MentalHealth {
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MentalHealth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mental_health);
+
+
+        VideoView videoView = (VideoView) findViewById(R.id.meditationVideo);
+        VideoView videoView2 = (VideoView) findViewById(R.id.breathExerciseVideo);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.meditation);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.breathing_exercise);
+
+
+        MediaController mediaController = new MediaController(this);
+        //link mediaController to videoView
+        mediaController.setAnchorView(videoView);
+        //allow mediaController to control our videoView
+        mediaController.setAnchorView(videoView2);
+        videoView.setMediaController(mediaController);
+        videoView2.setMediaController(mediaController);
+
+        videoView.start();
+        videoView2.start();
+
+
+    }
 }
