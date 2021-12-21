8e0d4b096b747502941c8fb278058649e3ebd112
U180709025 <uzayisinalici@posta.mu.edu.tr>
2021-12-21 10:10:51 +0300

    PhysicalHealth

--- app/src/main/java/com/example/myhealth/PhysicalHealth.java
+++ app/src/main/java/com/example/myhealth/PhysicalHealth.java
@@ -1,27 +1,32 @@
 package com.example.myhealth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.w3c.dom.Text;
+
 public class PhysicalHealth extends AppCompatActivity {
 
-    EditText distanceInKm ;
-    int distance ;
+    EditText txtDistance ;
     EditText stepsCount ;
-    int steps ;
     EditText ExtraBurnedCalories ;
-    int extraCalories ;
     EditText totalCalorie ;
-    int totalCalorieIntake ;
+    TextView distance ;
+    TextView steps ;
+    TextView extraCalorie ;
+    TextView totalCalorieIntake ;
+    int countOfSteps, kmCount, calorieCount ;
     FloatingActionButton backMain ;
-    Button OK_btn1 ;
-    Button OK_btn2 ;
-    Button OK_btn3 ;
+    Button btnENTER ;
 
 
     @Override
@@ -29,22 +34,48 @@ public class PhysicalHealth extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physical_health);
 
-        distanceInKm = (EditText) findViewById(R.id.txtDistanceInKm) ;
+        distance = (TextView) findViewById(R.id.txtDistance );
+        txtDistance = (EditText) findViewById(R.id.txtDistanceInKm);
+
+        steps = (TextView) findViewById(R.id.txtSteps) ;
         stepsCount = (EditText) findViewById(R.id.txtStepsCount) ;
+
+        extraCalorie = (TextView) findViewById(R.id.txtExtraCalorie);
         ExtraBurnedCalories = (EditText) findViewById(R.id.txtExtraBurnedCalories) ;
+
+        totalCalorieIntake = (TextView) findViewById(R.id.txtTotalCalorie) ;
         totalCalorie = (EditText) findViewById(R.id.txtTotalCalorieIntake) ;
-        OK_btn1 = (Button) findViewById(R.id.OKbtn) ;
-        OK_btn2 = (Button) findViewById(R.id.OKbtn2) ;
-        OK_btn3 = (Button) findViewById(R.id.OKbtn3) ;
+
+        btnENTER = (Button) findViewById(R.id.btnEnter) ;
+        btnENTER.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countOfSteps = Integer.parseInt(stepsCount.getText().toString());
+                kmCount =Integer.parseInt(distance.getText().toString());
+                calorieCount = Integer.parseInt(ExtraBurnedCalories.getText().toString());
+
+                showToast(countOfSteps);
+                showToast(kmCount);
+                showToast(calorieCount);
+            }
+        });
+
         backMain =(FloatingActionButton) findViewById(R.id.btnBacktoMain3) ;
+        backMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMain();
 
+            }
+        });
 
+    }
+    public void goToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
-
-
-
-
-
-
+    public void showToast (int text ) {
+        Toast.makeText(PhysicalHealth.this, text, Toast.LENGTH_SHORT).show();
     }
 }
